18ab21510ad4577671156f207837e086143e9b33
Daniel Hofmeister <de.slannesH@gmail.com>
2014-10-08 08:25:36 +0200

    Add stack & Queue

---
+++ src/queue.erl
@@ -0,0 +1,42 @@
+% Author: Daniel Hofmeister
+-module(queue).
+-export([createQ/0, front/1, enqueue/2, dequeue/1, isEmptyQ/1]).
+
+%createQ: es werden intern zwei neue ADT Stacks ohne Elemente angelegt. Einer
+%ist der Eingabestack, der andere der Ausgabestack
+%createQ -> Queue
+createQ() -> [stack:createS(),stack:createS()].
+
+%front: das zuerst hinzugefügte Element wird ausgegeben. Ist der Ausgabestack
+%leer, werden zuerst alle aus dem Eingabestack in den Ausgabestack umgestapelt.
+%front(Queue) -> Elem
+front([{},{}]) -> false;
+front([Input,Output]) -> case stack:isEmptyS(Output) of
+							true -> Restacked = restack(Input,Output),stack:top(Restacked);
+							false -> stack:top(Output)
+						 end.
+
+%restack -> StackxStack -> Stack						 
+restack({},Output) -> Output;
+restack(Input,Output) -> Elem = stack:top(Input), Inputnew = stack:pop(Input), Outputnew = stack:push(Output,Elem), restack(Inputnew,Outputnew).
+						  
+
+%enqueue: delegiert mit push wird das angegebene Element auf dem
+%Eingabestack abgelegt
+%enqueue(Queue,Elem) -> Queue
+enqueue([Input,_Out],Elem) -> NewInput = stack:push(Input,Elem),[NewInput,_Out].
+
+%dequeue: das zuerst hinzugefügte Element (das erste Element des
+%Ausgabestacks) wird entfernt. Ist der Ausgabestack leer, werden zuerst alle aus
+%dem Eingabestack in den Ausgabestack umgestapelt.
+%dequeue(Queue) -> Queue
+dequeue([Input,Output]) -> case stack:isEmptyS(Output) of
+							true -> Restacked = restack(Input,Output), OutputNew1 = stack:pop(Restacked), [{},OutputNew1];
+							false -> OutputNew2 = stack:pop(Output),[Input,OutputNew2]
+                           end.
+
+%isEmptyQ: delegiert mit isEmptyS geprüft, ob sich Elemente im Eingabestack
+%befinden
+%isEmptyQ: 
+isEmptyQ([{},{}]) -> true;
+isEmptyQ([_,_]) -> false.
---
+++ src/stack.erl
@@ -0,0 +1,23 @@
+% Author: Daniel Hofmeister
+-module(stack).
+-export([createS/0, push/2, pop/1, top/1, isEmptyS/1]).
+
+%createS: durch internes create von ADT Liste einen Stack anlegen
+%createS() -> Stack
+createS() -> liste:create().
+
+%push: mit insert an erster Position das Element in der Liste einfügen
+%push(Stack,Elem) -> Stack
+push(Stack,Elem) -> liste:insert(Stack,1,Elem).
+
+%pop: mit delete das Element an erster Stelle aus der Liste löschen
+%pop(Stack) -> Stack
+pop(Stack) -> liste:delete(Stack,1).
+
+%top: das Element an erster Stelle ausgeben
+%top(Stack) -> Elem
+top(Stack) -> liste:retrieve(Stack,1).
+
+%isEmptyS: delegieren an isEmpty der Liste
+%isEmptyS(Stack) -> bool
+isEmptyS(Stack) -> liste:isEmpty(Stack).
