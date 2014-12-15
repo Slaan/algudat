430e7f7eb126de79fed2eac17c70fddb6a4e0437
Slaan <de.SlannesH@gmail.com>
2014-12-15 20:00:01 +0100

    Small update to avl

--- src/avl.erl
+++ src/avl.erl
@@ -1,21 +1,31 @@
 -module(avl).
 -compile(export_all).
 
-init(A) -> {open,A,open}.
+init(A) -> {open,{A,0},open}.
 	
-add(Tree,A) -> 
-	{Left,Cur,Right} = Tree,
+add(Tree,A,Rotate) -> 
+	{Left,{Cur,OwnHeight},Right} = Tree,
+  NewHeight = OwnHeigt,
 	case (A<Cur) of
 		true -> case (Left==open) of
-					true -> NewLeft={open,A,open};
-					false -> NewLeft=add(Left,A)
+					true -> NewLeft={open,{A,0},open},
+                  NewHeight = OwnHeight-1;
+					false -> case OwnHeight of
+                   -1 = NewLeft=add(Left,A,true),rorate_right();
+                    0 = NewLeft=add(Left,A,false),NewHeight=-1;
+                    1 = NewLeft=add(Left,A,false),NewHeight=0
+                   end
 				end,
 				Result = {NewLeft,Cur,Right};
 		false -> case (Right==open) of
 					true -> NewRight={open,A,open};
-					false -> NewRight=add(Right,A)
+					false -> {Height,NewRight}=add(Right,A)
 				end,
 				Result = {Left,Cur,NewRight}
 		end,
-	Result.
+	{Height,Result}
+  case Height of
+      2 -> Result
 		
+
+
